75ba5c18b159e8235c176ec8cd3bdf3fe70f8df2
yxl1014 <1627419257@qq.com>
2020-10-12 20:05:07 +0800

    first commit

--- main.cpp
+++ main.cpp
@@ -112,6 +112,42 @@ int update_num(int num,Node * update){
         t=t->next;
     }
 }
+void select_all(){
+    Node *p=head;
+    while(p!=0){
+        cout<<"id:"<<p->num<<"  "<<"username:"<<p->username<<"  "<<"password:"<<p->password<<endl;
+        p=p->next;
+    }
+}
 int main() {
-    return 0;
+    int a=add_Node(1,"yu","123");
+    cout<<"run over:"<<a<<endl;
+    a=add_Node(2,"liu","456");
+    cout<<"run over:"<<a<<endl;
+    a=add_Node(3,"wang","789");
+    cout<<"run over:"<<a<<endl;
+    a=add_Node(4,"zhang","147");
+    cout<<"run over:"<<a<<endl;
+    a=add_Node(5,"gao","258");
+    cout<<"run over:"<<a<<endl;
+    a=add_Node(6,"ren","369");
+    cout<<"run over:"<<a<<endl;
+    a=add_Node(1,"qwe","234");
+    cout<<"run over:"<<a<<endl;
+    select_all();
+    a=delete_node(4);
+    cout<<"run over:"<<a<<endl;
+    a=delete_node(4);
+    cout<<"run over:"<<a<<endl;
+    select_all();
+    Node * aa=select_num(2);
+    cout<<"run over:"<<"id:"<<aa->num<<"  "<<"username:"<<aa->username<<"  "<<"password:"<<aa->password<<endl;
+    aa=select_up("yu","123");
+    cout<<"run over:"<<"id:"<<aa->num<<"  "<<"username:"<<aa->username<<"  "<<"password:"<<aa->password<<endl;
+    Node * update=(Node *)malloc(sizeof(Node));
+    update->username="yxl";
+    update->password="123456";
+    a=update_num(1,update);
+    cout<<"run over:"<<a<<endl;
+    select_all();
 }
